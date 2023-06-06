988a2f27050c278ec4eb2fe78b851108211aa433
MADINA <115165194+madina-ngom@users.noreply.github.com>
2023-06-06 16:47:09 +0200

    Add files via upload

---
+++ module-connexion/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="file:///C:/Users/mnbet/OneDrive/D%C3%A9veloppement%20web/module-connexion/module.html" rel="stylesheet"
+        type="text/css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>module connexion</title>
+</head>
+
+<body>
+    <form>
+        <div class="container">
+            <p>💻 Se Connecter 📱</p>
+            <input type="email" placeholder="Email ou Nom utilisateur"><br>
+            <input type="password" placeholder="Mot de passe"><br>
+            <input type="button" value="Connexion"><br>
+            <a href="#">Mot de passe oublié</a>
+    </form>
+    <div class="drop drop-1"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-3"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-5"></div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ module-connexion/inscription.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="file:///C:/Users/mnbet/OneDrive/D%C3%A9veloppement%20web/module-connexion/module.html" rel="stylesheet"
+        type="text/css">
+    <link rel="stylesheet" href="css/inscription.css">
+    <title>module connexion</title>
+</head>
+
+<body>
+    <form>
+        <div class="container">
+            <p>💻 Inscription 📱</p>
+            <input type="login" placeholder="Nom utilisateur"><br>
+            <input type="email" placeholder="Email"><br>
+            <input type="firstname" placeholder="Nom"><br>
+            <input type="lastname" placeholder="Prénom"><br>
+            <input type="password" placeholder="Mot de passe"><br>
+            <input type="mdp" placeholder="Confirmer mot de passe"><br>
+            <input type="button" value="Inscription 🎉"><br>
+            <a href="#"> </a>
+    </form>
+    <div class="drop drop-1"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-3"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-5"></div>
+    <div class="drop drop-6"></div>
+    <div class="drop drop-7"></div>
+    </div>
+
+
+</body>
+
+</html>
